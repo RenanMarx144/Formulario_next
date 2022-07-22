ff0af178af38d4a3f62a9ca5f8e4ce675505cc35
RenanMarx144 <renan.marx144@gmail.com>
2022-07-21 23:59:36 -0300

    Init commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Home() {
+    return (
+        <div>
+            <div>Home</div>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+    function AddCount() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={AddCount}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
